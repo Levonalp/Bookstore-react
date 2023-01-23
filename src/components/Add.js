a4d233cd5a654ff41809a95ab59cd8f3982f071a
levonal <lwshabee@gmail.com>
2023-01-23 01:23:10 -0600

    ready for monday

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 //only need props as a param if we are passing in props to this component (we are going to here).
 const Add = (props) => {
-  let emptyBook = { img:"",title: "", publisher: "", author: "", description:""};
+  let emptyBook = { title: "", publisher: "", author: ""};
   const [book, setBook] = useState(emptyBook);
 
   const handleChange = (event) => {
@@ -20,10 +20,10 @@ const Add = (props) => {
         <div className="row">
             <div className="col-12 col-md-8 offset-md-2 ">
                 <form onSubmit={handleSubmit}>
-                    <div className="form-group ">
+                    {/* <div className="form-group ">
                         <label htmlFor="img">img</label>
                         <input type="text" className="form-control" name="img" value={book.img} onChange={handleChange} />
-                    </div>
+                    </div> */}
                     <div className="form-group ">
                         <label htmlFor="title">Title</label>
                         <input type="text" className="form-control" name="title" value={book.title} onChange={handleChange} />
@@ -36,10 +36,10 @@ const Add = (props) => {
                         <label htmlFor="publisher">Publisher</label>
                         <input type="text" className="form-control" name="publisher" value={book.publisher} onChange={handleChange} />
                     </div>
-                    <div className="form-group">
+                    {/* <div className="form-group">
                         <label htmlFor="description">descritopn</label>
                         <input type="text" className="form-control" name="description" value={book.description} onChange={handleChange} />
-                    </div>
+                    </div> */}
                     <button type="submit" className="m-1 btn btn-primary">Submit</button>
                 </form>
             </div>
--- src/components/Book.js
+++ src/components/Book.js
@@ -15,12 +15,12 @@ const book = (props) => {
     return (
       <div className='card shadow-lg text-center m-3 '>
         <div className='card-body'>
-          <img src={props.book.img}/>
+          {/* <img src={props.book.img}/> */}
           <h5 className='card-title'>Title: {props.book.title}</h5>
           <hr className='my-4'/>
           <p className='card-text'>Author: {props.book.author}</p>
           <p className='card-text'>Publisher: {props.book.publisher}</p>
-          <p className='card-text'>description: {props.book.description}</p>
+          {/* <p className='card-text'>description: {props.book.description}</p> */}
           <Edit book={props.book} handleUpdate={props.handleUpdate} handleDelete={props.handleDelete}/>
         </div>
       </div>
